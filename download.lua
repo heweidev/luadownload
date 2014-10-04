b87a3427fee0cc271517d1cf59c3856cb5277fcc
hewei <netfield@163.com>
2014-10-04 10:51:21 +0800

    modify raw http and socket implement to fit coroutine call for yield can not be called in meta-method

--- download.lua
+++ download.lua
@@ -1,3 +1,5 @@
+dofile('./socket.lua')
+dofile('./http.lua')
 local http = require('socket.http')
 
 local MIN_BLOCK_SIZE = 128 * 1024
@@ -32,17 +34,59 @@ function random_file(ft, io_err)
 			
             if not chunk then
                 handle:close()
+				print('file closed.')
                 return 1
             else 
 				--print('get ', #chunk)
 				handle:seek('set', ft.offset)
 				ft.offset = ft.offset + #chunk
+				print('write at ', ft.offset, #chunk)
 				return handle:write(chunk)
 			end
         end
     else
 		return sink.error(io_err or "unable to open file") 
 	end
+end
+
+local
+function asyn_http_step(src, snk)
+	local function deal(chunk, src_err)
+		local ret, snk_err = snk(chunk, src_err)
+		if chunk and ret then return 1
+		else return nil, src_err or snk_err end
+	end
+	
+	local chunk, src_err, partial = src()
+	chunk = chunk or partial
+	
+	if src_err then
+		if chunk then
+			deal(chunk, src_err)
+		end
+		
+		if src_err == 'timeout' then
+			coroutine.yield(false)
+			return 1
+		elseif src_err == 'closed' then
+			return nil, src_err
+		end
+	else
+		return deal(chunk, src_err)
+	end
+	
+	--[[
+	if chunk then
+		--print(#chunk, src_err)
+		return deal(chunk, src_err)
+	else
+		print(src_err)
+		if src_err == 'timeout' then
+			coroutine.yield(false)
+			return 1
+		end
+	end
+	]]
 end
 
 local
@@ -58,13 +102,11 @@ function request(url, headers, savePath, offset)
 		url = url,
 		method = 'GET',
 		headers = headers,
-		sink = random_file(ft)
+		sink = random_file(ft),
+		step = asyn_http_step,
 	}
 	
-	if r ~= -1 then
-		print('http request error. err = ' .. status)
-	end
-	print('status = ', status)
+	print(r, c, h)
 end
 
 local threads = {}
@@ -81,18 +123,29 @@ function download(url, savePath)
 	--for i, v in pairs(h) do
 	--	print(i, v)
 	--end
+	
+	local co = coroutine.create(function()
+		request(url, headers, savePath)
+		return 'succ'
+	end)
+	
+	local count = 0
+	while true do
+		local ret, err = coroutine.resume(co)
+		print('resume', ret, err)
+		if not ret or err == 'succ' then	
+			break
+		end
+	end
 
-	local contentLength = tonumber(h['content-length'])
 	--[[
+	local contentLength = tonumber(h['content-length'])
 	local num, size = computeBlock(contentLength)
 	if num == 0 then
 		print('Content-Length not get!')
 		return
 	end
-	]]
-	num = 1
-	size = contentLength
-
+	
 	print(num, size)
 	for i = 1, num do
 		local co = coroutine.create(function()
@@ -105,13 +158,12 @@ function download(url, savePath)
 		end)
 		table.insert(threads, co)
 	end
+	]]
 end
 
 -- E151F93C6A993010B77546743108F80B
-local url = 'http://dlsw.baidu.com/sw-search-sp/soft/7b/26860/ThunderSpeed1.0.15.168.1411009942.exe'
+--local url = 'http://dlsw.baidu.com/sw-search-sp/soft/7b/26860/ThunderSpeed1.0.15.168.1411009942.exe'
 --local url = 'http://114.116.146.246/test.txt'
---local url = 'http://114.116.146.246/VA_X.dll'
+local url = 'http://localhost/VA_X.dll'	-- 69F5CC837061ECC535C5D0549673E26C
 download(url, DEFAULT_SAVEDIR .. 'test.exe')
-
-coroutine.resume(threads[1])
 
